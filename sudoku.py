f03cdd51ddaa692a72f14b273f3ea2cda491e30a
WhiteSeven7 <jiuwennitoufayouduochang@gmail.com>
2022-11-19 15:31:48 +0800

    输入功能基本完成

--- sudoku.py
+++ sudoku.py
@@ -1,10 +1,11 @@
 import tkinter as tk
+from typing import Union, Tuple, Dict
 
 num_font = ("黑体", 24)
 text_font = None
 
 
-def create_window(title: str, w, h, x=200, y=200):
+def create_window(title: str, w, h, x=500, y=150):
     ro = tk.Tk()
     ro.title(title)
     ro.resizable(False, False)
@@ -12,16 +13,28 @@ def create_window(title: str, w, h, x=200, y=200):
     return ro
 
 
+def click(self: 'MyLabel'):
+    def inner_func(event):
+        if app.control.select is self:
+            app.control.set(None)
+            self.label.config(background='green')
+            return
+        if app.control.select:
+            app.control.select.label.config(background='green')
+        app.control.set(self)
+        self.label.config(background='red')
+
+    return inner_func
+
+
 class MyLabel:
-    def __init__(self, master, row, column, value: int):
+    def __init__(self, master: tk.Misc, row: int, column: int, value: int):
         self.row = row
         self.column = column
         self.value = tk.StringVar(value=str(value))
-        self.label = tk.Label(master, font=num_font, textvariable=self.value, relief='raised')
+        self.label = tk.Label(master, font=num_font, textvariable=self.value, relief='raised', bg='green')
         self.label.config(width=2, height=1)
-
-    def layout(self):
-        self.label.grid(row=self.row, column=self.column, padx=1, pady=1)
+        self.label.bind('<Button-1>', click(self))
 
     def change(self, num: int):
         if num == 0:
@@ -29,33 +42,13 @@ class MyLabel:
         else:
             self.value.set(str(num))
 
-
-class MyFrame:
-    def __init__(self, master, row, column):
-        self.row = row
-        self.column = column
-        self.frame = tk.Frame(master, relief='groove', background=self.color())
-
-    def color(self):
-        return 'red' if (self.row + self.column) % 2 else 'white'
-
     def layout(self):
-        self.frame.grid(row=self.row, column=self.column, padx=3, pady=3)
-
-
-class Block:
-    def __init__(self, master, row, column, row_num, column_num):
-        self.my_frame = MyFrame(master, row, column)
-        self.my_label_dict = MyLabelDict(self.my_frame.frame, row_num, column_num)
-
-    def layout(self):
-        self.my_label_dict.layout()
-        self.my_frame.layout()
+        self.label.grid(row=self.row, column=self.column, padx=1, pady=1)
 
 
 class MyLabelDict:
-    def __init__(self, master, row_num, column_num):
-        self.group = {(row, column): MyLabel(master, row, column, 1)
+    def __init__(self, master: tk.Misc, row_num: int, column_num: int):
+        self.group = {(row, column): MyLabel(master, row, column, 0)
                       for row in range(row_num)
                       for column in range(column_num)}
 
@@ -64,8 +57,30 @@ class MyLabelDict:
             my_Label.layout()
 
 
+class MyFrame:
+    def __init__(self, master: tk.Misc, row: int, column: int):
+        self.row = row
+        self.column = column
+        self.frame = tk.Frame(master, relief='groove')
+
+    def layout(self):
+        self.frame.grid(row=self.row, column=self.column, padx=3, pady=3)
+
+
+class Block:
+    def __init__(self, master: tk.Misc, row: int, column: int, row_num: int, column_num: int):
+        self.my_frame = MyFrame(master, row, column)
+        self.my_label_dict = MyLabelDict(self.my_frame.frame, row_num, column_num)
+
+    def layout(self):
+        self.my_label_dict.layout()
+        self.my_frame.layout()
+
+
 class FrameDict:
-    def __init__(self, master, row_num, column_num, rn, cn):
+    group: Dict[Tuple[int, int], 'Block']
+
+    def __init__(self, master: tk.Misc, row_num: int, column_num: int, rn: int, cn: int):
         self.group = {(row, column): Block(master, row, column, rn, cn)
                       for row in range(row_num)
                       for column in range(column_num)}
@@ -75,9 +90,53 @@ class FrameDict:
             my_frame.layout()
 
 
-root = create_window("数独游戏", 600, 600)
-frame = tk.Frame(root)
-frame_dict = FrameDict(frame, 3, 3, 3, 3)
-frame_dict.layout()
-frame.pack(side='bottom')
-root.mainloop()
+class Window:
+    def __init__(self):
+        self.root = create_window("数独游戏", 500, 400)
+        self.frame = tk.Frame(self.root)
+        self.frame_dict = FrameDict(self.frame, 3, 3, 3, 3)
+        self.frame_dict.layout()
+        self.frame.pack(side='bottom')
+
+    def run(self):
+        self.root.mainloop()
+
+
+class Control:
+    def __init__(self):
+        self.select: Union['MyLabel', None] = None
+
+    def bind(self, window: 'Window'):
+        window.root.bind('<Key>', self.key_press())
+
+    def set(self, my_label: Union['MyLabel', None]):
+        self.select = my_label
+
+    def key_press(self):
+        def inner_func(event: tk.Event):
+            if not self.select:
+                return
+            try:
+                num = int(event.keysym)
+            except ValueError:
+                if event.keysym in ('BackSpace', 'space'):
+                    num = 0
+                else:
+                    return
+            self.select.change(num)
+
+        return inner_func
+
+
+class App:
+    def __init__(self):
+        self.window = Window()
+        self.control = Control()
+        self.control.bind(self.window)
+
+    def run(self):
+        self.window.run()
+
+
+app = App()
+app.run()
